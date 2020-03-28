8d64ac8945482d992bd9d5b9267eeecc743c2927
kwiatas83 <kwiatas83@gmail.com>
2020-03-28 11:24:19 +0100

    zadanie1

---
+++ src/main/java/przyklad2.java
@@ -0,0 +1,28 @@
+import util.DatabaseConnection;
+
+import java.sql.*;
+
+public class przyklad2 {
+    public static void main(String[] args) {
+
+//        String url = "jdbc:mysql://localhost:3306/shop?serverTimezone=CET";
+//        String user = "root";
+//        String password = "Kijas66085@";
+        String query = "Select * from address where add_city like \"%\"?\"%\";";
+        try (
+                PreparedStatement preStatement = DatabaseConnection.getInstance().getConnection().prepareStatement(query);
+        ) {
+            preStatement.setString(1,"Paris");
+            ResultSet resultSet = preStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.print( resultSet.getInt("ADD_ID")+" ");
+                System.out.print( resultSet.getString("ADD_CITY"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
